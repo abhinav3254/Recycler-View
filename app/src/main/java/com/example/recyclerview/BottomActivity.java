b9b8df6d8fabcb2285db3cd1ab95abfa52e39158
abhinav3254 <abhinav3254@gmail.com>
2023-01-02 11:56:42 +0530

    bottom sheet

--- app/src/main/java/com/example/recyclerview/BottomActivity.java
+++ app/src/main/java/com/example/recyclerview/BottomActivity.java
@@ -17,8 +17,6 @@ public class BottomActivity extends AppCompatActivity {
     MaterialButton country_btn;
 
     ArrayList<String> list;
-    BottomSheetAdapter adapter;
-    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,26 +36,6 @@ public class BottomActivity extends AppCompatActivity {
         list.add("Japan");
         list.add("Vietnam");
 
-
-
-        country_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(BottomActivity.this);
-                bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog);
-                recyclerView = findViewById(R.id.design_bottom_sheet_recycler_view);
-
-
-//                adapter = new BottomSheetAdapter(BottomActivity.this,list);
-//                recyclerView.setAdapter(adapter);
-//                recyclerView.setLayoutManager(new LinearLayoutManager(BottomActivity.this));
-
-
-
-                bottomSheetDialog.show();
-            }
-        });
 
     }
 }
